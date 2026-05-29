f1e3a72cbd5fac5699d7791e0dc47268693e533f
Luanzin26 <amanda8mart@gmail.com>
2026-05-29 15:36:43 -0300

    29/05

---
+++ Assets/Scripts/PersonagemController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersonagemController : MonoBehaviour
+{
+    public Rigidbody2D rb2d;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = this.GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb2d.velocity += new Vector2(1, 0);
+
+    }
+}
