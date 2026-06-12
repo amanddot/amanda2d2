1b1a0f4ebb93c285fafe94b8901f19a816dbafff
JairBolsonaro-King <amanda8mart@gmail.com>
2026-06-12 15:40:37 -0300

    12/06

--- Assets/Scripts/CoinController.cs
+++ Assets/Scripts/CoinController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class CoinController : MonoBehaviour
 {
-    private void ONTriggerEnter2D(Collider2D other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
