61892b416a9d201db9fdfbb987682fb42780dfa7
JairBolsonaro-King <amanda8mart@gmail.com>
2026-06-26 16:10:19 -0300

    atualizado fundo roxo

--- Assets/Scripts/CoinController.cs
+++ Assets/Scripts/CoinController.cs
@@ -3,13 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CoinController : MonoBehaviour
+
 {
+    public GameObject coinParticle;
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
-            Pontuacao pontuacao= other.GetComponent<Pontuacao>();
+            Pontuacao pontuacao = other.GetComponent<Pontuacao>();
             pontuacao.AddPoints();
+            Instantiate(coinParticle, this.transform.position, Quaternion.Euler(0, 0, 0));
             Destroy(this.gameObject);
         }
     }
